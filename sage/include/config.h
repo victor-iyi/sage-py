974d6d287171146899f84cadc135b504646db2e7
Victor I. Afolabi <javafolabi@gmail.com>
2019-01-09 14:20:52 +0100

    BUILD SUCCEEDED!!!

---
+++ sage/include/config.h
@@ -0,0 +1,10 @@
+// Project name.
+#define PROJECT_NAME "sage"
+
+// Project description.
+#define PROJECT_DESCRIPTION "A lightweight implementation of Knowledge Graph"
+
+// Version details.
+#define PROJECT_VERSION_MAJOR 1
+#define PROJECT_VERSION_MINOR 0
+#define PROJECT_VERSION_PATCH 0
