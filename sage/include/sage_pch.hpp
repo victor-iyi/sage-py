711f8e89de9e86b7b2647fd1ea1bc0bdfeb9c5e7
Victor I. Afolabi <javafolabi@gmail.com>
2019-01-10 19:36:26 +0100

    Updated pre-compiled headers

--- sage/include/sage_pch.hpp
+++ sage/include/sage_pch.hpp
@@ -1,23 +1,32 @@
 #ifndef SAGE_PCH_HPP
 #define SAGE_PCH_HPP
 
+// C-standard library.
 #include <cstdlib>
 #include <ctime>
 
+// C++ standard library.
 #include <algorithm>
 #include <functional>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <iterator>
 #include <memory>
 #include <utility>
 
+// Streams & containers.
 #include <sstream>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
+// Win32 API.
+#ifdef SAGE_PLATFORM_WINDOWS
+#include <Windows.h>
+#endif  // SAGE_PLATFORM_WINDOWS
+
+// Sage "static" includes.
 #include "sage/log.hpp"
 
 #endif  // !SAGE_PCH_HPP
